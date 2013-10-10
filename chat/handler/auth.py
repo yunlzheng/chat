34b16413135a51fbbca60dd837a08d4202b296b6
zhengyl <zhengyl@yun70.com.cn>
2013-10-10 02:19:18 -0700

    避免同一邮箱地址重复登录

--- chat/handler/auth.py
+++ chat/handler/auth.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import tornado.web
+from chat.define import ChatSigletonDefine
 from chat.handler import BaseHandler
 
 
@@ -11,6 +12,12 @@ class AuthHandler(BaseHandler):
     def post(self):
         nickname = self.get_argument("nickname")
         email = self.get_argument("email")
+
+        clients = ChatSigletonDefine.get_singleton_instance()._CLIENTS_MAP
+        for key in clients.keys():
+            if clients[key].email == email:
+                self.redirect("/?email=existed")
+                return 
         self.set_secure_cookie('email', email)
         self.set_secure_cookie('nickname', nickname)
         self.redirect("/chat")
@@ -20,12 +27,12 @@ class LogoutHandler(BaseHandler):
 
     @tornado.web.authenticated
     def get(self):
-            """
-            退出登录，清除当前cookie值
+        """
+        退出登录，清除当前cookie值
 
-            """
-            self.clear_cookie("nickname")
-            self.clear_cookie("email")
-            self.redirect("/")
+        """
+        self.clear_cookie("nickname")
+        self.clear_cookie("email")
+        self.redirect("/")
 
 
